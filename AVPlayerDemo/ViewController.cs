39497b6d555e76d09ecafe288a8d7d5009bc7419
Nuno Horta <nunobhorta@gmail.com>
2016-02-19 17:12:39 +0000

    added show/hide controls gesture

--- AVPlayerDemo/ViewController.cs
+++ AVPlayerDemo/ViewController.cs
@@ -2,6 +2,8 @@
 using UIKit;
 using AVFoundation;
 using Foundation;
+using CoreMedia;
+using System.Timers;
 
 namespace AVPlayerDemo
 {
@@ -22,6 +24,7 @@ namespace AVPlayerDemo
 		static NSString PlaybackLikelyToKeepUpContext = new NSString("PlaybackLikelyToKeepUpContext");
 
 		protected float RestoreAfterScrubbingRate, ScrubbingSpeed;
+		Timer touchTimer;
 
 		AVPlayer _player;
 		AVPlayerLayer _playerLayer;
@@ -42,6 +45,67 @@ namespace AVPlayerDemo
 		{
 			base.ViewDidLoad ();
 			LoadNotifications ();
+			AddControlsGesture ();
+
+			ControlsView.Hidden = true;
+		}
+
+		void AddControlsGesture()
+		{
+			var tap = new UITapGestureRecognizer ();
+			tap.AddTarget (ToggleControls);
+			tap.NumberOfTapsRequired = 1;
+			PlayerView.AddGestureRecognizer (tap);
+		}
+
+		void ToggleControls()
+		{
+			if (ControlsView.Hidden)
+				ShowToolbar ();
+			else
+				HideToolbar (null, null);
+
+			if(touchTimer != null)
+				touchTimer.Stop ();
+
+			touchTimer = new Timer(3000);
+			touchTimer.Elapsed += new ElapsedEventHandler(HideToolbar);
+			touchTimer.Enabled = true;
+		}
+
+		void ShowToolbar()
+		{
+			ControlsView.Hidden = false;
+
+			InvokeOnMainThread (delegate {
+				UIView.Animate (
+					0, 0, UIViewAnimationOptions.CurveEaseIn,
+					() => {
+						ControlsView.Alpha = 1f;
+					}, () => {
+					});
+			});
+		}
+
+		void HideToolbar(object sender, ElapsedEventArgs e)
+		{
+			if (!IsPlaying() || touchTimer == null)
+				return;
+
+			if(touchTimer != null)
+				touchTimer.Stop ();
+
+			InvokeOnMainThread (delegate {
+				UIView.Animate (
+					0.4, 0, UIViewAnimationOptions.CurveEaseOut,
+					() => {
+						if(ControlsView != null)
+							ControlsView.Alpha = 0f;
+					}, () => {
+						if(ControlsView != null)
+							ControlsView.Hidden = true;
+					});
+			});
 		}
 
 		public override void ViewDidLayoutSubviews ()
@@ -97,6 +161,24 @@ namespace AVPlayerDemo
 			}
 
 			_playerItem = new AVPlayerItem (_asset);
+
+//			_playerItem.AddObserver (this, (NSString)"status", NSKeyValueObservingOptions.New|NSKeyValueObservingOptions.Initial, StatusObservationContext.Handle);
+//			_playerItem.AddObserver (this, (NSString)"playbackBufferFull", NSKeyValueObservingOptions.New|NSKeyValueObservingOptions.Initial, PlaybackBufferFullContext.Handle);
+//			_playerItem.AddObserver (this, (NSString)"playbackBufferEmpty", NSKeyValueObservingOptions.New|NSKeyValueObservingOptions.Initial, PlaybackBufferEmptyContext.Handle);
+//			_playerItem.AddObserver (this, (NSString)"playbackLikelyToKeepUp", NSKeyValueObservingOptions.New|NSKeyValueObservingOptions.Initial, PlaybackLikelyToKeepUpContext.Handle);
+		}
+
+		void ReleasePlayerItem()
+		{
+//			if (_playerItem != null) {
+//				_playerItem.CancelPendingSeeks ();
+//				_playerItem.RemoveObserver (this, (NSString)"status");
+//				_playerItem.RemoveObserver (this, (NSString)"playbackBufferFull");
+//				_playerItem.RemoveObserver (this, (NSString)"playbackBufferEmpty");
+//				_playerItem.RemoveObserver (this, (NSString)"playbackLikelyToKeepUp");
+//
+//				_playerItem = null;
+//			}
 		}
 
 		void SetupControls()
@@ -164,6 +246,29 @@ namespace AVPlayerDemo
 			return RestoreAfterScrubbingRate != 0f || _player.Rate != 0f;
 		}
 
+		void SyncScrubber()
+		{
+			CMTime playerDuration = _playerItem.Duration;
+			if (playerDuration == CMTime.Invalid)
+			{
+				Scrubber.MinValue = 0.0f;
+				return;
+			}
+
+			double duration = _playerItem.Duration.Seconds;
+			if(!double.IsInfinity(duration) && !double.IsNaN(duration))
+			{
+				float minValue = Scrubber.MinValue;
+				float maxValue = Scrubber.MaxValue;
+
+				double time = _playerItem.CurrentTime.Seconds;
+
+				float value = (float)((maxValue - minValue) * time / duration + minValue);
+				Scrubber.SetValue (value, true);
+//				UpdateCurrentime ();
+			}
+		}
+
 		void AVPlayerItemPlaybackStalled(NSNotification notification)
 		{
 			Console.WriteLine ("AVPlayerItemPlaybackStalled!");
@@ -187,6 +292,8 @@ namespace AVPlayerDemo
 		void AVPlayerPlaybackDidFinish(NSNotification notification)
 		{
 			Console.WriteLine ("AVPlayerPlaybackDidFinish");
+			ReleasePlayerItem ();
+			ShowPlayButton ();
 		}
 
 		void AppDidEnterBackground(NSNotification notification)
